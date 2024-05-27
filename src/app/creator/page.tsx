28db113baec62f5663191455e11517b674606b3b
neilck <neil@chong-kit.com>
2024-05-27 14:01:27 -0700

    Added recommendation when no private key.

--- src/app/creator/page.tsx
+++ src/app/creator/page.tsx
@@ -1,19 +1,25 @@
 "use client";
 
+import { useAccountContext } from "@/context/AccountContext";
 import theme from "@/app/components/ThemeRegistry/theme";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
+import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import MuiNextLink from "../components/items/MuiNextLink";
 import { CommonLayout } from "../components/ComonLayout";
 import { CardHeading, CardSubHeading } from "../components/items/CardHeadings";
 
 export default function CreatorHome() {
+  const accountContext = useAccountContext();
+  const profile = accountContext.currentProfile;
+
   return (
     <CommonLayout developerMode={true}>
       <Box
         sx={{
           pt: 2,
+          maxWidth: "600px",
         }}
       >
         <Box
@@ -39,6 +45,42 @@ export default function CreatorHome() {
           </Typography>
           <Typography variant="body1" fontWeight={600}></Typography>
         </Box>
+        {profile.pubkey != "" && !profile.hasPrivateKey && (
+          <Box
+            sx={{
+              mt: 2,
+              p: 2,
+              width: "100%",
+              minHeight: "100px",
+              borderColor: theme.palette.grey[500],
+              borderRadius: "10px",
+              backgroundColor: theme.palette.background.paper,
+            }}
+          >
+            <Typography variant="h6" color="red">
+              Recommendation
+            </Typography>
+            <CardSubHeading>Create a new profile</CardSubHeading>
+
+            <Typography variant="body1" pt={1}>
+              You're logged in using a Nostr signing extension without a saved
+              private key.
+            </Typography>
+            <Typography variant="body1" pt={1}>
+              To automatically publish to relays, we recommend creating a
+              separate profile for issuing badges and managing groups.
+            </Typography>
+            <Typography variant="body1" fontWeight={500} pt={1}>
+              To add a new profile slect "Add profile" from top right avatar
+              menu.
+            </Typography>
+            <Typography pt={1}>
+              Alternatively, you can choose to{" "}
+              <Link href="/profile/edit">save the private key</Link> for this
+              profile.
+            </Typography>
+          </Box>
+        )}
       </Box>
     </CommonLayout>
   );
